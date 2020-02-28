7780a499106cc4a04429b05b4b9bb99bd65ce778
Pierre Sevestre <pierre.sevestre@hotmail.fr>
2020-02-28 15:55:04 +0100

    add argparse

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
 import os
 import matplotlib.pyplot as plt
 
+import argparse
+
 from dataset import KaggleDataset, iBug300WDataset
 from utils import pointwise_euclidean_distance
 from model import ASM
+
+MODELS = {"ASM": ASM}
+
+DATASETS = {"Kaggle": KaggleDataset,
+            "iBug": iBug300WDataset}
 
 # TODO : Resize iBug images to same resolution ?
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(description="Experiment main")
+
+    parser.add_argument("--model", type=str)
+    parser.add_argument("--dataset", type=str)
+    args = parser.parse_args()
+
+    model_name = args.model
+    dataset_name = args.dataset
+
+
     # Import data
+    print("Loading data..")
     data_path = os.path.join("data", "Kaggle")
-    dataset = KaggleDataset(data_path)
-    X, Y = dataset.load(min_index=0, max_index=100)
+    dataset = DATASETS[dataset_name](data_path)
+    X_train, X_val, X_test, Y_train, Y_val, Y_test = dataset.load(min_index=0, max_index=100)
 
     # Train model
-    model = ASM()
-    model.fit(X, Y)
+    print("Training model..")
+    model = MODELS[model_name]
+    model.fit(X_train, Y_train)
 
     # Predict & visualize
-    lm = model.predict(X)
-    dataset.matplotlib_visualize_landmark(X[0], lm[0])
+    print("Evaluating..")
+    lm = model.predict(X_val)
+    dataset.matplotlib_visualize_landmark(X_val[0], lm[0])
     plt.show()
 
     # Evaluate
-    dist = pointwise_euclidean_distance(Y, lm)
-    print(f"PED of model : {dist}")
+    dist = pointwise_euclidean_distance(Y_val, lm)
+    print(f"PED of model {model_name} on {dataset_name} : {dist}")
+
+
